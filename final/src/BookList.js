283775ae8dfd4aa32e1a469694eaf726a285fa41
elchinguliev <elchingulievv@gmail.com>
2024-03-03 01:56:29 +0400

    new

--- final/src/BookList.js
+++ final/src/BookList.js
@@ -3,9 +3,10 @@ import './App.css';
 import { Link } from "react-router-dom";
 
 
-function BookList() {
+function BookList({addToFavourites}) {
     const [books, setBooks] = useState([]);
     const [records, setRecords] = useState(books);
+    const [comments, setComments] = useState({});
 
   
     useEffect(() => {
@@ -15,13 +16,29 @@ function BookList() {
         .catch(error => console.error('Error fetching books:', error));
     }, []);
   
+    // const handleClick = () => {
+    //   addToFavourites({ books });
+    // };
+    const handleCommentAdd = (bookId, comment) => {
+      const updatedComments = { ...comments };
+      if (!updatedComments[bookId]) {
+        updatedComments[bookId] = [];
+      }
+      updatedComments[bookId].push(comment);
+      setComments(updatedComments);
+    };
+
+    const handleCommentRemove = (bookId, index) => {
+      const updatedComments = { ...comments };
+      updatedComments[bookId].splice(index, 1);
+      setComments(updatedComments);
+  };
     const Filter=(event)=>{
       setRecords(books.filter(f=>f.name.toLowerCase().includes(event.target.value)))
   }
 
     return (
       <div>
-
       <input type='next' className='form-control' onChange={Filter} placeholder='Search by lower letters of the book names'></input>
         <table style={{marginTop:"100px",marginLeft:"100px", wordSpacing:"1px"}}>
         <thead>
@@ -42,8 +59,37 @@ function BookList() {
               <td>{d.author}</td>
               <td>{d.publishDate}</td>
               <td>
-                <img style={{width:"200px" ,height:"150px"}} src={d.imageUrl}></img></td>
-              {/* Add more columns as needed */}
+                <img style={{width:"200px" ,height:"150px"}} src={d.imageUrl}></img>
+              </td>
+
+              <td>
+                  {/* Render comments for this book */}
+                  {/* {comments[d.id] && comments[d.id].map((comment, index) => (
+                  <div key={index}>
+                       {comment}
+                        <button style={{marginLeft:"100px"}} onClick={() => handleCommentRemove(d.id, index)}>Remove</button>
+                                    </div>
+                         ))}
+                        Add comment form */}
+                        <form onSubmit={(e) => {
+                        e.preventDefault();
+                        const newComment = e.target.comment.value;
+                       if (newComment.trim() !== '') {
+                         handleCommentAdd(d.id, newComment);
+                          e.target.reset();
+                           }
+                                }}>
+                       <input style={{width:"100%", fontSize:"20px", marginLeft:"5px"}} type="text" name="comment" placeholder="Add a comment" />
+                        <button className='addBtn'  type="submit">Add</button>
+                        {comments[d.id] && comments[d.id].map((comment, index) => (
+                  <div key={index}>
+                       {comment}
+                        <button className='removeBtn'  onClick={() => handleCommentRemove(d.id, index)}>Remove</button>
+                                    </div>
+                         ))}
+                          </form>
+              </td>
+
             </tr>
           ))}
         </tbody>
